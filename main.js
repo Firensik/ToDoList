96290d9c4ad9936e676e90fd9d0a5d2d8b9c5238
Firensik <firensik@gmail.com>
2022-10-12 15:39:23 +0200

    try

---
+++ main.js
@@ -0,0 +1,63 @@
+const toDoList = [];
+
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const taskNumber = document.querySelector('h1 span');
+const listItems = document.getElementsByClassName('task');
+const input = document.querySelector('input');
+const liElements = document.querySelectorAll('li');
+
+
+
+
+
+
+
+const removeTask = (e) => {
+    e.target.parentNode.remove();
+    const index = e.target.parentNode.dataset.key;
+    toDoList.splice(index, 1)
+    console.log(toDoList);
+    taskNumber.textContent = listItems.length;
+    renderList();
+}
+
+
+const addTask = (e) => {
+    e.preventDefault()
+    const titleTask = input.value;
+    if (titleTask === "") return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = titleTask + "<button>Usuń</button>";
+    toDoList.push(task)
+    renderList()
+
+    ul.appendChild(task);
+    input.value = "";
+    // const liItems = document.querySelectorAll('li.task').length;
+    taskNumber.textContent = listItems.length;
+    task.querySelector('button').addEventListener('click', removeTask);
+}
+
+
+
+const renderList = () => {
+    ul.textContent = "";
+    toDoList.forEach((toDoElement, key) => {
+        toDoElement.dataset.key = key;
+        ul.appendChild(toDoElement);
+    })
+}
+const searchTask = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    let tasks = toDoList;
+    tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText))
+    ul.textContent = '';
+    tasks.forEach(li => ul.appendChild(li))
+}
+
+
+
+form.addEventListener('submit', addTask)
+input.addEventListener('input', searchTask);
